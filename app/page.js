f71f18f06af528bdffa562145030d8230f6919f1
Burak Ülkü <brkulku@gmail.com>
2023-10-31 13:02:01 +0100

    Create final tailwind Create final tailwind

--- app/page.js
+++ app/page.js
@@ -1,4 +1,6 @@
-export default function Home() {
-  return <h1 className="underline">Hellodssd ld!</h1>;
-}
 // font-bold underline
+import 'tailwindcss/tailwind.css';
+
+export default function Home() {
+  return <h1 className="text-3xl font-bold"> Welcome! </h1>;
+}
